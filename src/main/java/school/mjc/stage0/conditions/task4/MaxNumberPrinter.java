60fbad36b1cfb361ded81a61ee7541a5f1d8aae0
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-05-01 22:12:29 +0300

    Ternary Conditions task solution

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int maxNumber = 0;
+        if (first >= second) {
+            maxNumber = first;
+        } else {
+            maxNumber = second;
+        }
+        if (maxNumber <= third) {
+            maxNumber = third;
+        }
+        System.out.println(maxNumber);
     }
 }
